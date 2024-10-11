04276020062750adffe208ee9b9cecc77daaa197
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-10-11 16:52:27 -0300

    rendering the proposal component when a new proposal is created

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,34 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
 {
   public Project $project;
 
+  public int $qty = 5;
+
+  #[Computed()]
+  public function proposals()
+  {
+    return $this->project->proposals()->orderBy('hours')->paginate($this->qty);
+  }
+
+  public function loadMore()
+  {
+    $this->qty += 10;
+  }
+
+  #[Computed()]
+  public function lastProposalTime()
+  {
+    return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+  }
+
+  #[On('proposal::created')]
   public function render()
   {
     return view('livewire.projects.proposals');
